5a08de951fb45be4a9f44a07162b938b32661965
Feyris-Tan <ft@feyris-tan.lebtimnetz.de>
2020-08-27 12:07:11 +0200

    Added a C# Port

---
+++ Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using CM3D2_DLC_CheckerSharp.Properties;
+using Microsoft.Win32;
+
+namespace CM3D2_DLC_CheckerSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(new string('=', 101));
+            Console.WriteLine("CM3D2_DLC_Checker");
+            Console.WriteLine(new string('=', 101));
+
+            RegistryKey registry = Registry.CurrentUser;
+            registry = registry.OpenSubKey("SOFTWARE");
+            registry = registry.OpenSubKey("KISS");
+            registry = registry.OpenSubKey("カスタムメイド3D2");
+            string installPath = registry.GetValue("InstallPath", ".").ToString();
+            DirectoryInfo cm3d2InstallPath = new DirectoryInfo(installPath);
+            if (!cm3d2InstallPath.Exists)
+            {
+                MessageBox.Show(String.Format("{0} does not exist.", cm3d2InstallPath.FullName));
+                return;
+            }
+
+            FileInfo cm3d2Exe = new FileInfo(Path.Combine(cm3d2InstallPath.FullName, "CM3D2x64.exe"));
+            if (!cm3d2Exe.Exists)
+            {
+                MessageBox.Show(String.Format("{0} was not found!", cm3d2Exe.FullName));
+                return;
+            }
+
+            StreamReader lstReader;
+            if (File.Exists("CM_ListDLC.lst"))
+                lstReader = new StreamReader(File.OpenRead("CM_ListDLC.lst"), Encoding.UTF8);
+            else
+                lstReader = new StreamReader(new MemoryStream(Resources.CM_ListDLC, false), Encoding.UTF8);
+            
+            List<DLC> installed = new List<DLC>();
+            List<DLC> uninstalled = new List<DLC>();
+            while (!lstReader.EndOfStream)
+            {
+                string cline = lstReader.ReadLine();
+                if (!cline.Contains(","))
+                    continue;
+
+                args = cline.Split(',');
+                DLC dlc = new DLC();
+                dlc.filename = args[0];
+                dlc.dlcName = args[1];
+                dlc.targetFile = new FileInfo(Path.Combine(cm3d2InstallPath.FullName, dlc.filename));
+
+                if (dlc.targetFile.Exists)
+                    installed.Add(dlc);
+                else
+                    uninstalled.Add(dlc);
+            }
+
+            installed.Sort((x, y) => x.dlcName.CompareTo(y.dlcName));
+            uninstalled.Sort((x, y) => x.dlcName.CompareTo(y.dlcName));
+
+            Console.WriteLine("Already Installed:");
+            installed.ForEach(x => Console.WriteLine(x.dlcName));
+
+            Console.WriteLine("\nNot Installed:");
+            uninstalled.ForEach(x => Console.WriteLine(x.dlcName));
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Press any key to end program");
+            Console.ReadKey();
+        }
+    }
+
+    class DLC
+    {
+        public string filename;
+        public FileInfo targetFile;
+        public string dlcName;
+
+        public override string ToString()
+        {
+            return dlcName;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# CM3D2_DLC_Checker
+# CM3D2_DLC_CheckerSharp
 Application that can sort between DLCs that have been installed and those that have not
+
+This is based on Tankerch's CM3D2_DLC_Checker. I had trouble getting it to run in python, so I ported it to C#.
